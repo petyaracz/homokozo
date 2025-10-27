92799e6cf6a01288e849a3e316d8df717aec4206
Péter Rácz <petyaracz@users.noreply.github.com>
2025-10-27 13:35:20 +0000

    monday

--- filter_nouns_further.r
+++ filter_nouns_further.r
@@ -4,3 +4,22 @@ d = read_tsv('noun.tsv.gz')
 
 d |> 
     filter(form == 'embereidnek')
+
+keep_words = d |> 
+    count(lemma,xpostag) |> 
+    count(lemma) |> 
+    filter(n == 16) |> 
+    pull(lemma)
+
+d2 = filter(d, lemma %in% keep_words)
+
+d2 |> 
+    filter(lemma == 'rész', str_detect(xpostag, 'Pl.Poss')) |> 
+    select(lemma, form, xpostag)
+
+d |> 
+    distinct(lemma, llfpm10) |> 
+    mutate(
+        ntile_llfpm10 = ntile(llfpm10, 3)
+    ) |> 
+    filter(ntile_llfpm10 == 2)

7cf5f516e46ee980a67f4b453c08938d92353265
Péter Rácz <petyaracz@users.noreply.github.com>
2025-10-27 11:49:51 +0000

    noun2

--- filtering_script_for_nouns.r
+++ filtering_script_for_nouns.r
@@ -2,11 +2,17 @@ library(tidyverse)
 
 d = read_tsv('https://github.com/petyaracz/Racz2024/raw/refs/heads/main/resource/webcorpus2freqlist/webcorpus2_freqlist_hu_with_lemmafreq.tsv.gz')
 
-egerek = d |> 
-    filter(str_detect(form,'^(egér|egérnek|egerek|egereknek|egerem|egereim|egereimnek|egeremnek|egere|egerei)$'))
+# egerek = d |> 
+#    filter(str_detect(form,'^(egér|egérnek|egerek|egereknek|egerem|egereim|# egereimnek|egeremnek|egere|egerei)$'))
 
-my_xpostags = egerek |> 
-    pull(xpostag)
+# my_xpostags = egerek |> 
+#     select(xpostag)
+
+# write_tsv(my_xpostags, 'noun_xpostags.tsv', col_names = FALSE)
+
+my_xpostags = read_tsv('noun_xpostags.tsv', col_names = FALSE) |> 
+    pull(X1) |> 
+    unique()
 
 d2 = d |> 
     filter(xpostag %in% my_xpostags)
@@ -15,11 +21,17 @@ dim(d2)
 
 d3 = d2 |> 
     filter(
-        freq > 10,
-        lemma_syl_count == 2,
-        nchar(lemma) == 6,
-        !str_detect(lemma, '[aáeéiíoóöőuúüű]$')
+        lemma_freq > 10,
+        lemma_syl_count <= 3,
+        nchar(lemma) <= 12,
+        !str_detect(lemma, '[aáeéiíoóöőuúüű]$'),
+        !str_detect(lemma, '[xw]')
         )
         
 d3 |> 
-    write_tsv('noun.tsv')
+    write_tsv('noun.tsv.gz')
+
+d3 |>
+    count(lemma) |> 
+    filter(n == 12) |> 
+    nrow()    
